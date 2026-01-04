7d8f791c8893915e714600b86656571094f4fcd8
Hamid Turky <haidturkey@gmail.com>
2026-01-04 19:50:22 +0300

    Update Vite config for build options

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // If you are deploying to username.github.io/repo-name/, 
-  // set base to '/repo-name/'. For username.github.io/, use '/'
   base: './',
   build: {
     outDir: 'dist',
-    sourcemap: true
+    emptyOutDir: true,
+    sourcemap: false
   }
 });
